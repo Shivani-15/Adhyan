b26b0fb4c271f2fec79d79fa37407518ac13f366
Shivani-15 <shpandey15@gmail.com>
2021-05-08 12:11:51 +0530

    New

--- adhyan/adhyan/settings.py
+++ adhyan/adhyan/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-41p#8i(aulz=p77z-*mx)=k6+m(@i**ij&^&f!387o+bj!l6z)'
+SECRET_KEY = 'django-insecure-$973tvp620ong@k#lqpjkfb5(jmky@xf11=r5t^hs(f3y2in_9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,9 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'study.apps.StudyConfig',
-    'users.apps.UsersConfig',
-    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -126,9 +123,3 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
-
-LOGIN_REDIRECT_URL = 'study-home'
-
-LOGIN_URL = 'login'
