470de2b85399b6e54f24c205927f3999f699c36c
Madhumita V <madhumitav24@gmail.com>
2021-05-08 15:07:12 +0530

    Added Login/Register

---
+++ adhyan/users/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .forms import UserRegistrationForm, UserUpdateForm
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username=form.cleaned_data.get('username')
+            messages.success(request,f'Account successfully created!')
+            return redirect('login')
+    else:
+        form=UserRegistrationForm()
+    return render(request, 'users/register.html', {'form': form})
